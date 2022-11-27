b799c4c256c19310ec5b93e959e12ea0ffa66828
yu nishisaki <ynishisaki@gmail.com>
2022-11-27 22:45:37 +0900

    add:webpack.configに解説コメント追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,55 @@
 // The path to the CesiumJS source code
 const cesiumSource = "node_modules/cesium/Source";
 const cesiumWorkers = "../Build/Cesium/Workers";
+
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
+    // ベースとなるディレクトリ（デフォルトのカレントディレクトリのまま）
     context: __dirname,
+    // エントリーポイントの設定（デフォルトのまま）
     entry: {
         app: "./src/index.js",
     },
+    // ビルドしたファイルの出力場所
     output: {
         filename: "app.js",
         path: path.resolve(__dirname, "dist"),
         sourcePrefix: "",
     },
+    // amd: {
+    //     // Enable webpack-friendly use of require in Cesium
+    //     toUrlUndefined: true,
+    // },
     resolve: {
         fallback: { https: false, zlib: false, http: false, url: false },
         mainFiles: ["index", "Cesium"],
     },
     module: {
+        // 各ローダーの設定
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: true,
+                        },
+                    },
+                ],
             },
+            // これちゃんと機能してるかよくわからない
             {
                 test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-                use: ["url-loader"],
+                type: "asset/inline",
             },
         ],
     },
@@ -36,7 +57,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "src/index.html",
         }),
-        // Copy Cesium Assets, Widgets, and Workers to a static directory
         new CopyWebpackPlugin({
             patterns: [
                 { from: path.join(cesiumSource, cesiumWorkers), to: "Workers" },
@@ -49,6 +69,9 @@ module.exports = {
             CESIUM_BASE_URL: JSON.stringify(""),
         }),
     ],
+    // webpackの--modeと同じ
     mode: "development",
-    devtool: "eval",
+    // webpackの--devtoolと同じ。evalはソースマップを生成しない
+    // devtool: "eval",
+    devtool: "source-map",
 };
