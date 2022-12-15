16b763eff75c28a5dc7b0867179576d588f99b25
yu nishisaki <ynishisaki@gmail.com>
2022-12-15 17:59:06 +0900

    add:現在地を取得しカメラ初期位置を設定

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ var viewer = new Cesium.Viewer("cesiumContainer", {
     }),
 });
 
+// ジオイド面用にTerrainを追加（EGM96）
+const tileset = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: Cesium.IonResource.fromAssetId(1434440),
+    })
+);
+
 // G空間情報センターに置かれている、プロジェクトPlateauで作成した、オルソ画像タイルの参照
 var imageProvider = new Cesium.UrlTemplateImageryProvider({
     url: "https://gic-plateau.s3.ap-northeast-1.amazonaws.com/2020/ortho/tiles/{z}/{x}/{y}.png",
@@ -22,13 +29,79 @@ var current_image =
     viewer.scene.imageryLayers.addImageryProvider(imageProvider);
 
 // 東京都千代田区の建物データ（3D Tiles）
-var your_3d_tiles = viewer.scene.primitives.add(
+// minato-ku
+var your_3d_tiles_Minato = viewer.scene.primitives.add(
     new Cesium.Cesium3DTileset({
-        url: "https://plateau.geospatial.jp/main/data/3d-tiles/bldg/13100_tokyo/13101_chiyoda-ku/notexture/tileset.json",
+        url: "https://plateau.geospatial.jp/main/data/3d-tiles/bldg/13100_tokyo/13103_minato-ku/notexture/tileset.json",
+    })
+);
+// shinagawa-ku
+
+var your_3d_tiles_shinagawa = viewer.scene.primitives.add(
+    new Cesium.Cesium3DTileset({
+        url: "https://plateau.geospatial.jp/main/data/3d-tiles/bldg/13100_tokyo/13109_shinagawa-ku/notexture/tileset.json",
     })
 );
 
-// カメラの初期位置の指定
-viewer.camera.setView({
-    destination: Cesium.Cartesian3.fromDegrees(139.76, 35.68, 5000.0),
+// const seaLevelRise = viewer.entities.add({
+//     name: "標高0m",
+//     polygon: {
+//         height: 45, //EGM96: 39.37 Meters
+//     },
+//     position: Cesium.Cartesian3.fromDegrees(139.756389, 35.637222, 0.0),
+
+//     box: {
+//         dimensions: new Cesium.Cartesian3(5000.0, 5000.0, 1.0),
+//         material: Cesium.Color.CYAN.withAlpha(0.3),
+//     },
+// });
+
+// The height, in meters, above the ellipsoid.
+
+const seaLevelRise = viewer.entities.add({
+    name: "virtual sea level rise",
+    rectangle: {
+        coordinates: Cesium.Rectangle.fromDegrees(139.7, 35.61, 139.78, 35.65),
+        material: Cesium.Color.CYAN.withAlpha(0.3),
+        // rotation: Cesium.Math.toRadians(45),
+        // extrudedHeight: 3776.0,
+        // extrudedHeight: 136.4424, // 日本のジオイド2011より算出36.4424
+        // EGM96:39.37
+        height: 39,
+    },
 });
+
+// カメラの初期位置を設定
+const setCameraView = (initialLat, initialLon) => {
+    viewer.camera.setView({
+        destination: Cesium.Cartesian3.fromDegrees(
+            initialLon,
+            initialLat,
+            5000.0
+        ),
+    });
+};
+
+function getCurrentPositionIsSuccess(position) {
+    const initialLat = position.coords.latitude;
+    const initialLon = position.coords.longitude;
+    setCameraView(initialLat, initialLon);
+}
+
+function getCurrentPositionIsError() {
+    console.log("位置取得できませんでした、カメラをデフォルト位置に設定します");
+    const initialLat = 35.6451607;
+    const initialLon = 139.7552528;
+    setCameraView(initialLat, initialLon);
+}
+
+// 位置情報API
+if (navigator.geolocation) {
+    console.log("Locating…");
+    navigator.geolocation.getCurrentPosition(
+        getCurrentPositionIsSuccess,
+        getCurrentPositionIsError
+    );
+} else {
+    console.log("Geolocation is not supported by your browser");
+}
